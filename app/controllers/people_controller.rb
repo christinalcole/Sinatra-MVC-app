3dffff43fd3c175c89593c42967707c205541f6b
Christina L. Cole <christinalcole@gmail.com>
2016-01-19 19:26:57 -0500

    Adding show page for individual person in db

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,3 +2,13 @@ get '/people' do
   @people = Person.all
   erb :"/people/index"
 end
+
+get '/people/:id' do
+  @person = Person.find(params[:id])
+  birthdate_string = @person.birthdate.strftime("%m%d%Y")
+  number = Person.first_num(birthdate_string)
+  path_number = Person.nn(number)
+  @message = Person.birth_path_msg(path_number)
+
+  erb :"people/show"
+end
